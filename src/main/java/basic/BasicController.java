6129f84aeea571f6116149bc263fe21c2b8b64df
Bhaskar S <bhaskar@Bhaskars-MacBook-Air.local>
2017-08-28 22:09:28 +0530

    Skeletal structure for the project

---
+++ src/main/java/basic/BasicController.java
@@ -0,0 +1,14 @@
+package basic;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class BasicController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
